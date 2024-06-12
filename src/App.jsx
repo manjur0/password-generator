4e23462b1c96930043d8742912f2dcb830e97926
manjur0 <sfmanjur21@gmail.com>
2024-06-12 12:18:16 +0600

    Password Generator React app initialized

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <h1 className="text-4xl text-center font-bold my-8">
+        Password Generator
+      </h1>
+    </div>
+  );
+}
+
+export default App;
